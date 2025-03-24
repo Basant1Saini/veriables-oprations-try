09ae35902e03705755ec1dc65acca3392adb974d
basant saini <basant357@gmail.com>
2025-03-24 06:47:40 +0530

    Add initial HTML and JavaScript files with basic content and logging

---
+++ javascript.js
@@ -0,0 +1,32 @@
+console.log('hello world!');
+
+let name = 'John';
+let surname = 'Doe';
+console.log(name);
+console.log(surname);
+
+let age = 25;
+console.log(age);
+
+age = 30;
+console.log(age);
+
+const pi = 3.14;
+// pi =10;
+console.log(pi);
+
+console.log (3+2)-76*(1+1);
+console.log (23+97);
+console.log ((4+6+9)/77);
+
+let a = 10;
+console.log(a);
+
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+console.log(percentage);
+
